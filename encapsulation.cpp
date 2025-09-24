d6a2df2a68b9e526e39281d923e4b825cb2047ed
Sairam Cr <crsairam7617@gmail.com>
2025-09-24 23:11:12 +0530

    encapsulation example

---
+++ encapsulation.cpp
@@ -0,0 +1,51 @@
+
+// wrapping data and its methods under a single unit called class
+
+//helps us with data hiding and abstraction by using private variables 
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+
+    //properties (atributes)
+
+private:
+    double sal; //lets say you dont want to change the salary of the teacher directly from main
+
+public:
+
+    string name;
+    string dept;
+    string sub;
+    //methods (functions)
+
+    void changedept(string newdept){
+         dept=newdept;
+    }
+
+    //getter for getting the private variable within the class
+    void getsal(double s){
+        sal=s;
+    }
+
+    //setter for setting the private variable within the class and returning it
+    double setsal(){
+        return sal;
+    }
+
+};
+
+int main(){
+    Teacher t1; //object for first teacher in the teacher system
+    t1.name="John";
+    t1.sub="Maths";
+    t1.dept="Science";
+    t1.getsal(5000);
+    cout<<t1.setsal()<<endl;
+
+    cout<<t1.name<<endl;
+    return 0;
+}
